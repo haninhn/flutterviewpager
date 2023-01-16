92961004edd67f87dcbfa972f740219944ead1bb
HanineGuesmi <GuesmiHanin@takiacademyteam.com>
2023-01-16 15:49:26 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,82 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.Dart';
 void main() {
-  runApp(const MyApp());    
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp(      
-    {super.key});           
-  @override       
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: TabBar(
+                indicator: BoxDecoration(
+                    color: const Color(0xff344054),
+                    borderRadius: BorderRadius.circular(20.0)),
+                labelColor: Colors.white,
+                unselectedLabelColor: const Color(0xff344054),
+                tabs: const [
+                  Tab(
+                    text: 'Lessons',
+                  ),
+                  Tab(
+                    text: 'Exams',
+                  ),
+                  Tab(
+                    text: 'Recording',
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+                child: TabBarView(
+              children: [
+                Center(
+                  child: Text("Lessons Pages"),
+                ),
+                Center(
+                  child: Text("Exams Pages"),
+                ),
+                Center(
+                  child: Text('Recording Page'),
+                ),
+              ],
+            ))
+          ],
+        ),
+      )),
+    );
+  }
+}
